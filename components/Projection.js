9ea60ae75175f7db4c5531b70fa61328e9e95658
Tamer Sali <tamerrsali@gmail.com>
2023-06-22 00:09:51 +0300

    reworked section

--- components/Projection.js
+++ components/Projection.js
@@ -1,47 +1,3 @@
-
-const movieData = {
-	"movie_id": 1,
-	"movie_name": "The Witcher",
-	"is_popular": 1,
-	"cover": "https://www.pixelstalk.net/wp-content/uploads/images6/Witcher-HD-Wallpaper-Free-download.jpg",
-	"is_3d": 1,
-	"status_id": 2,
-	"is_published": 1,
-	"parent_control_id": 3,
-	"duration": 183,
-	"created_at": "2023-05-11 10:03:03",
-	"updated_at": "2023-05-11 10:03:03",
-	"status_name": "active",
-	"parent_control_code": "PG-13",
-	"trailer": "https://youtu.be/ndl1W4ltcmg",
-	"actors": [
-		{
-			"actor_id": 1,
-			"actor_name": "Hayden Christensen",
-			"actor_image": "https://m.media-amazon.com/images/M/MV5BY2Y2NTE4YzYtMTA4OS00NzNmLWIxNzctMDEyMjE2NWU3YWNmXkEyXkFqcGdeQXVyODMwMzI4MTg@._V1_QL75_UX280_CR0,0,280,280_.jpg",
-			"in_the_role_of": "Anakin Skywalker"
-		},
-		{
-			"actor_id": 2,
-			"actor_name": "Natalie Portman",
-			"actor_image": "https://m.media-amazon.com/images/M/MV5BYzU0ZGRhZWItMGJlNy00YzlkLWIzOWYtNDA2NzlhMDg3YjMwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_QL75_UX280_CR0,24,280,280_.jpg",
-			"in_the_role_of": "Padme"
-		},
-		{
-			"actor_id": 3,
-			"actor_name": "Ewan McGregor",
-			"actor_image": "https://m.media-amazon.com/images/M/MV5BMTg1MjQ0MDg0Nl5BMl5BanBnXkFtZTcwNjYyNjI5Mg@@._V1_QL75_UX280_CR0,22,280,280_.jpg",
-			"in_the_role_of": "Obi-Wan Kenobi"
-		},
-		{
-			"actor_id": 4,
-			"actor_name": "Samuel L. Jackson",
-			"actor_image": "https://m.media-amazon.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_QL75_UX280_CR0,25,280,280_.jpg",
-			"in_the_role_of": "Mace Windu"
-		}
-	]
-}
-
 const Actor = {
 	name: "actor",
 	props: ["actor"],
@@ -73,41 +29,43 @@ const Projection = {
 	template: `
         <div class='projection'>
 			<div class="container">
-				<img
+				<iframe
+					v-if="movie.movie_trailer"
+					width="100%"
+					height="100%"
+					:src="youtubeEmbed(movie.movie_trailer)"
+					:title="movie.movie_name"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+					allowfullscreen
+				>
+				</iframe>
+				<div class="projection-info">
+					<img
 					:src="movie.cover"
 					:alt="movie.movie_name"
-					width="1920"
-					height="1080"
+					width="500"
+					height="500"
 				/>
-				<div class="projection-info">
 					<div class="info">
 						<h1>{{movie.movie_name}}</h1>
 						<p>
 							<svg width="15" height="12" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg" fill="orange">
 								<path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"/>
 							</svg>
-							8.2 | {{ movie.duration }} • {{ movie.parent_control_code }}
+							{{ movie.movie_rating }} | {{ transformDuration(movie.duration) }} • {{ movie.parent_control_code }}
 						</p>
- 						<button class="reserve-button" @click="clickHandler">Reserve</button>
-						<button	v-if="movie.trailer" class="trailer-button" @click="playTrailer = !playTrailer">Trailer</button>
+						<p class="movie-description">{{ movie.movie_description }}</p>
+ 						<router-link
+						 to="/"
+						 v-slot="{href, route, navigate}"
+						>
+						 <button :href="href" @click="navigate" class='reserve-button'>
+							Reserve
+						 </button>
+						</router-link>
 					</div>
  				</div>
-				<div v-if="playTrailer" class="trailer-overlay" @click="playTrailer = false">
-					<button @click="playTrailer = false">+</button>
-				</div>
- 				<div :class="['trailer', {active: playTrailer}]" ref="trailer">
-					<iframe
-						v-if="playTrailer && movie.trailer"
-						width="300"
-						height="300"
-						:src="movie.trailer"
-						:title="movie.movie_name"
-						frameborder="0"
-						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-						allowfullscreen
-					>
-					</iframe>
-				</div>
 			</div>
 
 
@@ -121,14 +79,15 @@ const Projection = {
 		const route = VueRouter.useRoute();
 		const playTrailer = Vue.ref(false)
 		const trailer = Vue.ref(null);
+		const movie = Vue.ref({})
 
-		const id = route.params.id
-		const movie = movieData.movie_id === +id ? movieData : null
-
-		const youtubeEmbed = movie?.trailer && `https://www.youtube.com/embed/${movie.trailer.split('/').pop()}`
-
-		const clickHandler = () => {
-			router.push('/')
+		const youtubeEmbed = (trailer) => {
+			const url = new URL(trailer);
+			const searchParams = new URLSearchParams(url.search);
+			if(searchParams){
+				return `https://www.youtube.com/embed/${searchParams.get('v')}?autoplay=1&mute=1`
+			}
+			return "https://www.youtube.com/embed/"
 		}
 
 		const transformDuration = (duration) => {
@@ -140,10 +99,11 @@ const Projection = {
 			return `${rhours}h ${rminutes}m`;
 		}
 
-
-		movie.duration = transformDuration(movie.duration)
-		movie.trailer = youtubeEmbed;
-
+		Vue.onBeforeMount(async () => {
+			const res = await fetch(`${apiBaseUrl}/movie/view/?id=${route.params.id}`)
+			const data = await res.json();
+			movie.value = data;
+		});
 
 		Vue.watch(playTrailer, (flag, prevCount) => {
 			if (flag) {
@@ -155,14 +115,14 @@ const Projection = {
 			} else {
 				document.body.style.overflow = ''
 			}
-		})
-
+		});
 
 		return {
 			movie,
+			transformDuration,
+			youtubeEmbed,
 			playTrailer,
 			trailer,
-			clickHandler
 		}
 	},
 };
